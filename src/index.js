de64ce746f0cde52be5117cf6d89fc782ae94d59
AleksIvanovSinglet <Aleksandar.Ivanov@singlet.dev>
2021-10-29 11:50:28 +0000

    arrange code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 const core = require('@actions/core');
-const { github } = require('@actions/github');
 const { request } = require('@octokit/request');
 
 try {
+	getDefaultBranch().then((data) => {
+		console.log(`default branch is ${data}`);
+		core.setOutput("default_branch", data);
+	});
+
 	async function getDefaultBranch() {
   		const origin = core.getInput('origin');
 		const repositoryPayload = await request(`GET /repos/${origin}`);
 		const defaultBranch = await repositoryPayload.data.default_branch;
 		return defaultBranch;
 	}
-
-	getDefaultBranch().then((data) => {
-		console.log(`default branch is ${data}`);
-		core.setOutput("default_branch", data);
-	});
-
-	// const payload = JSON.stringify(github.context.payload, undefined, 2)
-
-  	// console.log(`The event payload: ${payload}`);
 } catch (error) {
   	core.setFailed(error.message);
 }
